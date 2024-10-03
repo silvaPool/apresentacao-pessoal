463454deb819db933f0369edafa9f4b2ee70eecf
silvaPool <silva.friends22@gmail.com>
2024-10-03 09:23:01 -0300

    arquivo data para armazenar as informacoes

---
+++ src/services/data.js
@@ -0,0 +1,11 @@
+const info = [
+    {
+        nome: "Matheus Silva",
+        descricao: "Texto de descricao",
+        habilidades: "Habilidades",
+        projetosRecentes: "Projetos recentes",
+        redesSociais: "Links de redes sociais"
+    }
+]
+
+export default info;
