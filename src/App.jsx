fa1a7aed87a26e8e2ff074f03088a5427a39fb25
silvaPool <silva.friends22@gmail.com>
2024-10-04 09:06:35 -0300

    estilizacao da secao redes - 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Habilidades from "./components/Habilidades";
 import Projetos from "./components/Projetos";
+import Redes from "./components/Redes";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Habilidades />
       <Projetos />
+      <Redes />
     </>
   );
 }
